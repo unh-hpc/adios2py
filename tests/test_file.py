68700a45ecd68beef8607f7a0e993e0a90585ce6
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-01-04 11:48:24 -0500

    tests: check reading back test1 file, using low-level API

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+from typing import Any
 
 import adios2.bindings as ab  # type: ignore[import-untyped]
 import numpy as np
@@ -33,9 +34,50 @@ def test1_file(tmp_path):
     return filename
 
 
+def _adios2_to_dtype(type_str: str) -> np.dtype[Any]:
+    if type_str.endswith("_t"):
+        type_str = type_str[:-2]
+
+    return np.dtype(type_str)
+
+
+def check_test1_file_lowlevel(filename: os.PathLike[Any] | str) -> None:
+    ad = ab.ADIOS()
+    io = ad.DeclareIO("io-test1")
+    engine = io.Open(os.fspath(filename), ab.Mode.ReadRandomAccess)
+    assert engine
+
+    var = io.InquireVariable("test_int")
+    assert var
+    dtype = _adios2_to_dtype(var.Type())
+    assert dtype == np.int64
+    size = var.Count()
+    assert size == []
+    shape = var.Shape()
+    assert shape == []
+    shape = tuple(shape)
+    test_int = np.empty(shape, dtype=dtype)
+    engine.Get(var, test_int)
+    assert test_int == 99
+
+    var = io.InquireVariable("test_floats")
+    assert var
+    dtype = _adios2_to_dtype(var.Type())
+    assert dtype == np.float64
+    size = var.Count()
+    assert size == [5]
+    shape = var.Shape()
+    assert shape == [5]
+    shape = tuple(shape)
+    test_floats = np.empty(shape, dtype=dtype)
+    engine.Get(var, test_floats)
+    assert np.all(test_floats == np.arange(5.0))
+
+
 def test_write_test1_file(test1_file):
     """Checks that test1_file is properly written.
 
     And at the same time, that it can be read using the low-level API.
     """
     assert test1_file
+    check_test1_file_lowlevel(test1_file)
